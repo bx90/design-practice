021854720d381ee52db1b8eb7223e38c93eb4852
bx_test_name <boxiong.sun@gmail.com>
2017-09-16 10:31:22 -0700

    add comment

--- pattern/src/main/java/creational/factory/Main.java
+++ pattern/src/main/java/creational/factory/Main.java
@@ -1,6 +1,9 @@
 package creational.factory;
 
 
+/**
+ * Factory
+ */
 public class Main {
     public static void main(String[] args) {
         Algorithm algorithm = AlgorithmFactory.createAlgorithm(AlgorithmFactory.SEARCH);
